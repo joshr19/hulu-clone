b8df942f94c05fc5aebf7f862dbc4ab6a8ffc3ae
joshr19 <joshuaradeen.19ee048@mbcet.ac.in>
2021-09-21 13:06:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Header from './Header';
+import Nav from './Nav';
+import Results from './Results';
+import requests from './requests';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [selectedOption,setSelectedOption] = useState(requests.fetchTrending);
+
+    return (
+        <div className="app">
+            <Header />
+          <Nav setSelectedOption={setSelectedOption} />
+   
+
+        <Results selectedOption = {selectedOption} />
+        
+
+        </div>
+    )
 }
 
-export default App;
+export default App
